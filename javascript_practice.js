16283ee382161a4e84bcec035aeca43c4de09d01
Ahkeem Lang <langahkeem@gmail.com>
2016-11-30 11:31:37 -0600

    Following instructions for javascript challenge

---
+++ javascript_practice.js
@@ -0,0 +1,18 @@
+const ACCESSORY_PRICE = 9.99;
+const PHONE_PRICE = 99.99;
+const TAX_RATE = 0.08;
+const SPENDING_THRESHOLD = 250;
+
+var bank_balance = 500.14;
+var amount = 0;
+
+function calculateFinalPurchaseAmount(amt) {
+  amt = amt + (amt * TAX_RATE);
+
+  return amt;
+}
+
+var amount = 99.99;
+
+amount = calculateFinalPurchaseAmount(amount);
+console.log(amount.toFixed(2));
