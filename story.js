1a8a0c054d4cca6708665ebda5ea2aa0d9d4ad1c
Ahkeem Lang <langahkeem@gmail.com>
2017-02-06 09:42:47 -0600

    Added a new file called story to exploit my current understanding of JavaScript

---
+++ story.js
@@ -0,0 +1,8 @@
+var adjective = prompt('Please add a adjective');
+  var sentence = "There was once a " + adjective;
+var verb = prompt('Please add a verb');
+  sentence += " programmer he was ready to " + verb;
+var noun = prompt('Please add a noun');
+  sentence += " the " + noun;
+alert('All done here!');
+document.write(sentence);
