2a8940f32e8cbea60af061ceb525f4ee22d43887
Ahkeem Lang <langahkeem@gmail.com>
2017-02-06 08:55:58 -0600

    Practicing with string concatenation

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 /* A JavaScript statement*/
-//This will execute on the screen...
-var greetings = "Hello! I'm Ahkeem!";
-var farewell = "Bye!";
-alert(greetings);
+//This will execute on the screen..
+var visitor = prompt("What is your name?")
+//String concatenation...
+var message = "Hello " + visitor + ". Welcome to my site.";
+message += " We are so glad that you came by to visit, ";
+message += visitor;
+message += ". Please come again when you wnat to learn some more.";
+document.write(message)
 //Document represents the current webpage...
 //Write is a command that writes a message to the page...
 document.write("<h1>Welcome to Javascript Basics</h1>");
-alert(farewell);
 //outputs messages to the console...
-console.log("Program complete");
+console.log("Program complete, you're reviewing a lot!");
