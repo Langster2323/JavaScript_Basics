95a1df57979e234ec0802bbb8b3ffb64dc75ecb1
Ahkeem Lang <langahkeem@gmail.com>
2017-02-08 08:22:51 -0600

    Finding out how many seconds I have lived

--- script.js
+++ script.js
@@ -22,5 +22,7 @@ var minsPerHour = 60;
 var hoursPerDay = 24;
 var daysPerWeek = 7;
 var weeksPerYear = 52;
-var secondsPerDay = secondsPerMin * minsPerHour *hoursPerDay;
+var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
 document.write('There are ' + secondsPerDay + ' seconds in a day');
+var yearsAlive = secondsPerDay * minsPerHour * hoursPerDay * daysPerWeek *
+weeksPerYear;
