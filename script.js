c0b4f1e1f840a5f7f3d1922f488320916d523661
Ahkeem Lang <langahkeem@gmail.com>
2017-02-08 08:17:50 -0600

    Printed the result out on to the page.

--- script.js
+++ script.js
@@ -23,3 +23,4 @@ var hoursPerDay = 24;
 var daysPerWeek = 7;
 var weeksPerYear = 52;
 var secondsPerDay = secondsPerMin * minsPerHour *hoursPerDay;
+document.write('There are ' + secondsPerDay + ' seconds in a day');
