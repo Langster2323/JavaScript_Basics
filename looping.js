43e2a8706b70b993bb28cd4ebe8f738c690d661a
Ahkeem Lang <langahkeem@gmail.com>
2017-02-10 08:58:03 -0600

    Discovered the correct answer to the looping challenge

--- looping.js
+++ looping.js
@@ -1,6 +1,11 @@
-
+/**** Incorrect ****/
 for (var triangle = 0; triangle += 1; var symbol = "#"){
   if (triangle <= 7)
   break
 }
 console.log(symbol);
+
+/**** Correct ****/
+
+for (var line = "#"; line.length < 8; line += "#")
+  console.log(line);
