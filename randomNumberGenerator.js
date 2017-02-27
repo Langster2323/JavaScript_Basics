862eade870e8d7e06bd46caa222aac53b21ee508
Ahkeem Lang <langahkeem@gmail.com>
2017-02-27 12:11:00 -0600

    Updated by adding more flexibility by creating else if statements

--- randomNumberGenerator.js
+++ randomNumberGenerator.js
@@ -3,10 +3,19 @@ let randomNumber = Math.floor(Math.random() * 6 ) + 1;
 let guess = prompt('I am thinking of a number between 1 and 6. What is it?');
 if (parseInt(guess) === randomNumber ) {
   correctGuess = true;
+} else if (parseInt(guess) < randomNumber ) {
+  let guessMore = prompt('Try again. The number I am thinking of is more than ' + guess);
+  if (parseInt(guessMore) ===  randomNumber){
+    correctGuess = true;
+  }
+} else if (parseInt(guess) > randomNumber ) {
+  let guessLess = prompt('Try again. The number I am thinking of is less than ' + guess);
+  if (parseInt(guessLess) === randomNumber) {
+    correctGuess = true;
+  }
 }
-
-if (correctGuess) {
-  document.write('<p>You guessed the number!</p>');
+if ( correctGuess ) {
+    document.write('<p>You guessed the number!</p>');
 } else {
-  document.write('<p>Sorry. THe number was ' + randomNumber + '.</p>');
+    document.write('<p>Sorry. The number was ' + randomNumber + '.</p>');
 }
