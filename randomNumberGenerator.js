89c6d22de3653311703efee655840c9fff1146b6
Ahkeem Lang <langahkeem@gmail.com>
2017-02-27 11:57:08 -0600

    Updated my file

--- randomNumberGenerator.js
+++ randomNumberGenerator.js
@@ -1,7 +1,12 @@
-var randomNumber = Math.floor(Math.random() * 6) +1;
-var guess = prompt("I am thinking of a number between 1 and 6?");
-if (parseInt(guess) === randomNumber) {
-  document.write("<p>You guessed the number!</p>");
+let correctGuess = false;
+let randomNumber = Math.floor(Math.random() * 6 ) + 1;
+let guess = prompt('I am thinking of a number between 1 and 6. What is it?');
+if (parseInt(guess) === randomNumber ) {
+  correctGuess = true;
+}
+
+if (correctGuess===true) {
+  document.write('<p>You guessed the number!</p>');
 } else {
-  document.write("<p>Sorry. The number was " + randomNumber + "</p>");
+  document.write('<p>Sorry. THe number was ' + randomNumber + '.</p>');
 }
