64523196ddffd4f0751be80394052eacc041435c
Ahkeem Lang <langahkeem@gmail.com>
2017-02-27 11:54:19 -0600

    misspelled thinking

--- randomNumberGenerator.js
+++ randomNumberGenerator.js
@@ -1,5 +1,5 @@
 var randomNumber = Math.floor(Math.random() * 6) +1;
-var guess = prompt("I am thinkg of a number between 1 and 6?");
+var guess = prompt("I am thinking of a number between 1 and 6?");
 if (parseInt(guess) === randomNumber) {
   document.write("<p>You guessed the number!</p>");
 } else {
