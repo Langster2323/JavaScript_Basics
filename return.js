2a8aec57903fb5244a067d8bad0ff9ccac350c49
Ahkeem Lang <langahkeem@gmail.com>
2017-02-27 16:39:36 -0600

    reminding myself about returning statements

---
+++ return.js
@@ -0,0 +1,10 @@
+//Return statements...
+
+function isEmailEmpty () {
+  let field = document.getElementById('email');
+  if (field === '') {
+    return true;
+  } else {
+    return false;
+  }
+}
