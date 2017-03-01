07cf2b9bb5fb1f9c85ea907fdcaab7d7b91383c5
Ahkeem Lang <langahkeem@gmail.com>
2017-03-01 09:55:50 -0600

    I wanted to practice passing in arguments into the function

--- random.js
+++ random.js
@@ -6,3 +6,10 @@ function getRandomNumber() {
 alert(getRandomNumber());
 console.log(getRandomNumber());
 var dieRoll = getRandomNumber();
+
+//Passing in arguments for the area of a rectangle...
+function getArea(width, length, unit) {
+  var area = width * length;
+  return area + '' + unit;
+}
+console.log(getArea(10, 20, 'sq ft') );
