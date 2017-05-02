447b05a47598d4412aefee619747b5154a4c4580
Ahkeem Lang <langahkeem@gmail.com>
2017-05-01 21:13:52 -0500

    combing loops and arrays

--- playlist.js
+++ playlist.js
@@ -10,3 +10,13 @@ var playList = [
 function print(message) {
   document.write(message);
 }
+
+function printList ( list ) {
+  var listHTML - '<ol>';
+  for ( var i = 0; i < list.length; 1 += 1) {
+    listHTML += '<li>' + list[i] + '</li>'
+  }
+  listHTML += '</ol>';
+  print(listHTML);
+}
+printList(playList);
