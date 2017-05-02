ef18ad01815da92e9080daf1d5e4d61e47bcfbba
Ahkeem Lang <langahkeem@gmail.com>
2017-05-01 21:07:36 -0500

    Created a dummy playlist

---
+++ playlist.js
@@ -0,0 +1,12 @@
+var playList = [
+  'I Did It My Way',
+  'Respect',
+  'Imagine',
+  'Born to Run',
+  'Louie Louie',
+  'Maybellene'
+];
+
+function print(message) {
+  document.write(message);
+}
