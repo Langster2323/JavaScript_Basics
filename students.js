58defc98aee2e8eaf8e71f934a8ab12e5627d55e
Ahkeem Lang <langahkeem@gmail.com>
2017-05-31 13:31:34 -0500

    Working on solutions on a challenge

---
+++ students.js
@@ -0,0 +1,36 @@
+var students = [
+  { name: "Ahkeem",
+    track: "Front End Development",
+    achivements: 95,
+    points: 8520
+  },
+
+  { name: "Ai",
+    track: "Web Design",
+    achivements: 83,
+    points: 5000
+  },
+
+  { name: "Nate",
+    track: "Python",
+    achivements: 97,
+    points: 9000
+  },
+
+  { name: "Cameron",
+    track: "Front End Development",
+    achivements: 55,
+    points: 2000
+  },
+
+  { name: "Anna",
+    track: "Web Design",
+    achivements: 30,
+    points: 1500
+  },
+  { name: "Greg",
+    track: "C+",
+    achivements: 86,
+    points: 7800
+  }
+];
