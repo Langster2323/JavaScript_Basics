d332e2ff3172d6cb2c94f7c7f6346c6aac8ca040
Ahkeem Lang <langahkeem@gmail.com>
2017-06-02 07:45:04 -0700

    Created a new fiel to separate specific concerns

---
+++ student_report.js
@@ -0,0 +1,19 @@
+var message = "";
+var student;
+
+function print(message) {
+  var outputDiv = document.getElementById('output');
+ outputDiv.innerHTML = message;
+}
+
+
+for(var i = 0; i < student.length; i += 1){
+  student = students[i];
+  message += '<h2>Student:  ' + student.name + '</h2>';
+  
+  students[i].name;
+  students[i].track;
+  students[i].achivements;
+  students[i].points;
+}
+print(message);
