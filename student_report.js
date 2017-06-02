455e88b9e99bf4654ffda0405c6c48e0678081c3
Ahkeem Lang <langahkeem@gmail.com>
2017-06-02 07:53:29 -0700

    Wrote out a message to be printed out on to the page

--- student_report.js
+++ student_report.js
@@ -10,10 +10,8 @@ function print(message) {
 for(var i = 0; i < student.length; i += 1){
   student = students[i];
   message += '<h2>Student:  ' + student.name + '</h2>';
-  
-  students[i].name;
-  students[i].track;
-  students[i].achivements;
-  students[i].points;
+  message += '<p>Track:  ' + student.track + '</p>';
+  message += '<p>Achivements:  ' + student.achivements + '</p>';
+  message += '<p>Points:  ' + student.points + '</p>';
 }
 print(message);
